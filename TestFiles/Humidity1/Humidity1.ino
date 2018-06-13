92073d0df8c5bd0de05c8664172eb83d9c37a7ee
Andreas T. Eggesvik <andreas.t.eggesvik@hotmail.com>
2018-06-13 14:42:21 +0200

    Added functions
    Functions for sleep, and sensors for humidity, pressure and temperature.

---
+++ TestFiles/Humidity1/Humidity1.ino
@@ -0,0 +1,22 @@
+#include "Adafruit_Sensor.h"
+#include "Adafruit_AM2320.h"
+
+Adafruit_AM2320 am2320 = Adafruit_AM2320();
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(10); // hang out until serial port opens
+  }
+
+  Serial.println("Adafruit AM2320 Basic Test");
+  am2320.begin();
+  delay(100);
+}
+
+void loop() {
+  Serial.print("Temp: "); Serial.println(am2320.readTemperature());
+  Serial.print("Hum: "); Serial.println(am2320.readHumidity());
+
+  delay(2000);
+}
---
+++ TestFiles/SleepMode2/SleepMode2.ino
@@ -0,0 +1,47 @@
+#include "LowPower.h"
+// This file requieres the LowPower support functions
+
+// Code for temperature sensor
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11 
+#define BMP_CS 10
+Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+////////////////////////////////////////
+
+void setup() {
+  // Code for temperature sensor
+  Serial.begin(9600);
+  
+  if (!bme.begin()) {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+  /////////////////////////////////////
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10);
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  ADCSRA |= (1 << 7); // Enabeling ADC
+  delay(100);
+
+  // Code for temperature setup
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+    
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure());
+  Serial.println(" Pa");
+
+  Serial.println();
+  delay(100); // Delay in order to print all to terminal
+  //////////////////////////////////
+}
