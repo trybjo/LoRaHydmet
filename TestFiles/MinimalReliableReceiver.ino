c5c1feaf96c884f71cdc0f1e2227c0db72b45bb4
Andreas T. Eggesvik <andreas.t.eggesvik@hotmail.com>
2018-06-18 16:30:25 +0200

    Updated for compatability with repeater

--- TestFiles/MinimalReliableReceiver.ino
+++ TestFiles/MinimalReliableReceiver.ino
@@ -21,39 +21,45 @@ void setup() {
 
 void loop() {
   if (manager.available()){
-    
-    // Receiving messages
     uint8_t bufLen = sizeof(buf); // Needs to be here to convert to uint8_t
     uint8_t from;
+    int receiveSuccess = 0;
     if (!manager.recvfromAck(buf, &bufLen, &from)){ //Function changes value of buf, bufLen and from
-      Serial.println("Receive unsuccessful");
+      Serial.println("Receive unsuccessful, was message not for me?");
+      receiveSuccess = 0;
     }
     else{
+      receiveSuccess = 1;
       Serial.print("Received message from 0x");
       Serial.print(from, HEX);
-      Serial.println(": ");
-      Serial.println((char*)buf);    
+      Serial.print(": ");
+      Serial.print((int)buf[0]);
+      Serial.println((char*)&buf[1]);    
     }
+    // After receiving one message, there may be more incoming
     int receivingBit = 1; // 1 when receiving
     int timeout = 700; // ms
     while (receivingBit){
       if (!manager.recvfromAckTimeout(buf, &bufLen, timeout, &from)){ // Listening for incoming messages 
-       Serial.println("Receive failed, assuming no more messages for this time");
+       Serial.println("Assuming no more messages for this time");
        receivingBit = 0;
       }
       else{
        Serial.print("Received message from 0x");
        Serial.print(from, HEX);
-       Serial.println(": ");
-       Serial.println((char*)buf);    
+       Serial.print(": ");
+       Serial.print((int)buf[0]);
+       Serial.println((char*)&buf[1]);    
       }
     } 
 
-    // After a successful receive(s), send a message: 
-    uint8_t data[] = "And hello back to you";
-    if (!manager.sendtoWait(data, sizeof(data), from)){
-      Serial.println("Sending failed");
+    // After successful receive(s), send a message: 
+    if (receiveSuccess){
+      uint8_t data[] = "And hello back to you";
+      if (!manager.sendtoWait(data, sizeof(data), from)){
+        Serial.println("Sending failed");
+      }
+      else Serial.println("Sending successful");
     }
-    else Serial.println("Sending successful");
-    }
+  }
 }
