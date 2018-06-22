56c619968f42d8551defa37727dfddb8095d2096
Garrux <36224866+Garrux@users.noreply.github.com>
2018-06-22 13:16:02 +0200

    GPS code that do not require the SoftwareSerial library

---
+++ TestFiles/GPS.ino
@@ -0,0 +1,29 @@
+#include "TinyGPS++.h"
+
+TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
+void setup()
+{
+  Serial.begin(9600);//This opens up communications to the Serial monitor in the Arduino IDE
+  Serial.println("GPS Start");//Just show to the monitor that the sketch has started
+}
+
+void loop()
+{
+  
+  while(Serial.available())//While there are characters to come from the GPS
+  {
+    gps.encode(Serial.read());//This feeds the serial NMEA data into the library one char at a time
+  }
+  if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
+  {
+  
+    //Get the latest info from the gps object which it derived from the data sent by the GPS unit
+    Serial.println("Satellite Count:");
+    Serial.println(gps.satellites.value());
+    Serial.println("Latitude:");
+    Serial.println(gps.location.lat(), 6);
+    Serial.println("Longitude:");
+    Serial.println(gps.location.lng(), 6);
+    delay(2000);
+ }
+}
