4023339722ac30a1bd484cb798b83809c02bfdb4
Garrux <36224866+Garrux@users.noreply.github.com>
2018-06-22 13:12:46 +0200

    Confirms received messages

--- TestFiles/Driftssentral.py
+++ TestFiles/Driftssentral.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 15 13:10:41 2018
-
-@author: Andreas
-"""
-
-# Need to use telnet. You must first enable telnet
+# Need to use telnet. Enable telnet if not already enabled.
 
 import socket
-import sys
 
 # Library for encryption/decryption
 from Crypto.Cipher import AES
@@ -33,12 +26,12 @@ def decryptMessage(encryptedMessage):
 # The host is here blank. This means that we can listen on any interface we choose.
 host = " "
 
-# Choose a free port
-port = 8888
+# Choose a free port.
+port = 8889
 
 # Can use try: and except: like in the client, but not necessary.
 
-# Create a TCP socket using IPv4
+# Create a TCP socket using IPv4.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket is created")
 
@@ -47,8 +40,9 @@ print("Socket is created")
 try:
     s.bind((host,port))
 except socket.error:
-    print("Binding failed")
-    sys.exit()
+    s.bind((host,port))
+    #print("Binding failed")
+    #sys.exit()
     
 print("Socket has been bounded")
 
@@ -64,27 +58,44 @@ conn, addr = s.accept()
 # addr[0] is the IP address, and addr[1] is the port number.
 print("Connected with " + addr[0] + " on port " + str(addr[1]))
 
+# A dummy string that is not equal to the first message, so that the message will be stored.
+oldData = "Dummy string"
+
 # Create an ininite loop where you listen for data from the client.
-while 1:
-    # We can recieve data from the client if it tries to send something.
+while True:
+    
+    # We recieve data from the client if it tries to send something.
     # Specify how many bytes you want to recieve. Here 1 kilobyte is used.
     data =  conn.recv(1024)
-    # print(data.decode())
-    print("\nEncrypted data:")
-    print(data)
-    print("\nDecrypted data:")
-    print(decryptMessage(data).decode())
+    if data != oldData and data:
+        print("\nEncrypted data:")
+        print(data)
+        print("\nDecrypted data:")
+        decryptedMessage = decryptMessage(data).decode()
+        print(decryptedMessage)
+        
+        # Save the data into a specified file.
+        f = open(r'''C:\Users\Andreas\Documents\Agder_Energi_S2018\Codes\Log.txt''', "a")
+        f.write(decryptedMessage)
+        f.write("\n")
+        f.close()
+        
+        oldData = data
+        
+        # Send confirmation for recieved data
+        confirmation = "k"
+        conn.sendall(confirmation.encode())
     
-    # Break out of the loop if the connection is broken.
+    # If the connection is broken, try to connect again.
     if not data:
-        break;
+        s.listen(10)
+        conn, addr = s.accept()
 
-    # We can send this message onwards to all the connected clients.
-    #telconn.sendall(data)
-
+'''
 # Close the socket connection between this server and the client.
 conn.close()
 print("Lost connection to the client. The connection is now closed. The server will also close now.")
 
 # Close the socket itself.
 s.close()
+'''
