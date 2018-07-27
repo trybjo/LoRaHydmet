c561526dc5210deef1d1cd85f4617fc0d71cee34
Garrux <36224866+Garrux@users.noreply.github.com>
2018-07-27 14:30:00 +0200

    Minor changes, works better now

--- TestFiles/UploadToDrive.py
+++ TestFiles/UploadToDrive.py
@@ -10,29 +10,66 @@ from apiclient.http import MediaFileUpload
 from time import sleep
 
 while True:
-    SCOPES = 'https://www.googleapis.com/auth/drive'
-    store = file.Storage('storage.json')
-    creds = store.get()
-
-    folderID = '1xD4LNtqT1ezVXa-x0KJd7V9tISEm1KIo'
-
-
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    DRIVE = discovery.build('drive', 'v2', http=creds.authorize(Http()))
-
-    FILES = (
-        ('dokument.txt', False),
-    )
-
-    for filename, convert in FILES:
-        metadata = {'title': 'dokument.txt', "parents": [{"id": folderID, "kind": "drive#childList"}]}
-        #metadata = {'title': filename,
-        #            'parents': folderID,}
-        res = DRIVE.files().insert(convert=convert, body=metadata,
-                media_body=filename, fields='mimeType,exportLinks').execute()
-        if res:
-            print('Uploaded "%s" (%s)' % (filename, res['mimeType']))
+    
+    try:
+        SCOPES = 'https://www.googleapis.com/auth/drive'
+        store = file.Storage('storage.json')
+        creds = store.get()
+    
+        folderID = '1xD4LNtqT1ezVXa-x0KJd7V9tISEm1KIo'
+    
+    
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+            creds = tools.run_flow(flow, store)
+        DRIVE = discovery.build('drive', 'v2', http=creds.authorize(Http()))
+    
+        FILES = (
+            ('Log.txt', False),
+        )
+    
+        for filename, convert in FILES:
+            metadata = {'title': 'Log.txt', "parents": [{"id": folderID, "kind": "drive#childList"}]}
+            #metadata = {'title': filename,
+            #            'parents': folderID,}
+            res = DRIVE.files().insert(convert=convert, body=metadata,
+                    media_body=filename, fields='mimeType,exportLinks').execute()
+            if res:
+                print('Uploaded "%s" (%s)' % (filename, res['mimeType']))
+        sleep(10800) # Seconds
         
-    sleep(60) # Seconds
+    except:
+        connected = False
+        print("Disconnected")
+        while not connected:
+            try:
+                SCOPES = 'https://www.googleapis.com/auth/drive'
+                store = file.Storage('storage.json')
+                creds = store.get()
+            
+                folderID = '1xD4LNtqT1ezVXa-x0KJd7V9tISEm1KIo'
+            
+            
+                if not creds or creds.invalid:
+                    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+                    creds = tools.run_flow(flow, store)
+                DRIVE = discovery.build('drive', 'v2', http=creds.authorize(Http()))
+            
+                FILES = (
+                    ('Log.txt', False),
+                )
+            
+                for filename, convert in FILES:
+                    metadata = {'title': 'Log.txt', "parents": [{"id": folderID, "kind": "drive#childList"}]}
+                    #metadata = {'title': filename,
+                    #            'parents': folderID,}
+                    res = DRIVE.files().insert(convert=convert, body=metadata,
+                            media_body=filename, fields='mimeType,exportLinks').execute()
+                    if res:
+                        print('Uploaded "%s" (%s)' % (filename, res['mimeType']))
+                sleep(10800) # Seconds
+                connected = True
+        
+            except:
+                sleep(5)
+            
