69e94c15ed36cb1acd5fa3919a22c089edbd016d
Andreas T. Eggesvik <andreas.t.eggesvik@hotmail.com>
2018-08-06 14:06:28 +0200

    Prints all possible data

---
+++ FinalSystem/ReliableReceiver/ReceiverCommunication.ino
@@ -0,0 +1,158 @@
+
+void initializeLoRa()
+{
+  //Set pinMode
+  pinMode(LoRa_RST, OUTPUT);
+  pinMode(LoRa_INT, OUTPUT);
+
+  // Manual reset of LoRa module
+  digitalWrite(LoRa_RST, HIGH); 
+  delay(100);
+  digitalWrite(LoRa_RST, LOW);
+  delay(10);
+  digitalWrite(LoRa_RST, HIGH);
+  delay(10);
+  digitalWrite(LoRa_INT, HIGH);
+  delay(100);
+  digitalWrite(LoRa_INT, LOW);
+  delay(10);
+  digitalWrite(LoRa_INT, HIGH);
+  delay(10);
+  pinMode(LoRa_INT, OUTPUT);
+
+  // Initialize both LoRa and Reliable Datagram
+  if (!manager.init())
+  Serial.println(F("Init failed"));
+  delay(1000);
+
+  // Initialize LoRa.
+  //  lora.init();
+
+  // Set frequency.
+  lora.setFrequency(LoRa_FREQ);
+  
+  // Set transmitter power, value from 7-23 (23 = 20 dBm). 13 dBm is default. 
+  // Can go above this using PA_BOOST, which RFM9x has.
+  lora.setTxPower(23);
+
+  // Set spreding factor, bandwidth and coding rate. 
+  // See LoRa #define in top of code for description and usage.
+//  LoRa.setSpreadingFactor(spreadingFactor); // This causes failure
+  LoRa.setSignalBandwidth(signalBandwidth);
+  LoRa.setCodingRate4(codingRateDenominator);
+  delay(200);
+}
+
+
+// The function calls receive function with or without timeout
+// Detects duplicates and update package memory
+// message, bufLen and from are called by reference
+// This means their values are updated after function call
+bool receive(bool* duplicate, uint8_t* message, uint8_t* bufLen, uint8_t* from, int timeout){
+  bool success;
+  if (timeout == -1){    
+     success = manager.recvfromAck(buf, bufLen, from);
+  }
+  else {
+     success = manager.recvfromAckTimeout(buf, bufLen, timeout, from);
+  }
+  
+  if (success){
+    if (messageIsDuplicate(buf, *from)){
+      // New message, we are interested
+      *duplicate = true;
+    }
+    else {
+      *duplicate = false;
+    }
+    return true;
+  }
+  else{
+    *duplicate = false;
+    return false;
+  }
+}
+
+// This function aims to save SRAM by not using Serial.print(float)
+// Replaces uint8PosToLongInt and uint8PosToFloat, and the Serial calls.
+
+// Decoding uint8_t* to integer or decimal values and writing to serial.
+// When 'decimals' == 0, the integer value will be printed.
+void sPrintData(uint8_t* input, int usedSize, int startPos, int decimals){
+  long int _temp = 0;
+  for (int i = startPos; i< startPos + usedSize; i++){
+    _temp += (long int)input[i] * toPowerOf(256, i-startPos); 
+  }
+  if (!decimals){
+    Serial.print(_temp);
+  }
+  else{
+    long int tenPower = toPowerOf(10,decimals);
+    Serial.print(_temp/tenPower); // Integer value
+    Serial.print(F("."));
+    Serial.print(_temp - tenPower*(_temp/tenPower)); // Decimals
+  }
+}
+
+
+void writeDataToSerial(uint8_t* message, int len, uint8_t from){
+  //sPrintData(input, usedSize, startPos, decimals);
+  
+  // Signal strength dBm
+  Serial.print((int)lora.lastRssi()); // This one need to be changed still
+  Serial.print(F(","));
+  // From: 
+  Serial.print(from);
+  Serial.print(F(","));
+  // Time: 
+  sPrintData(message, 4, 11, 0);
+  Serial.print(F(","));
+  // Packet number:
+  sPrintData(message, 1, 0, 0);
+  Serial.print(F(","));
+  // Temperature:    
+  sPrintData(message, 2, 1, 2);
+  Serial.print(F(","));
+  // Humidity:
+  sPrintData(message, 2, 3, 1);
+  Serial.print(F(","));
+  // Pressure:
+  sPrintData(message, 3, 5, 0);
+  Serial.print(F(","));
+  // Debth:
+  sPrintData(message, 3, 8, 0);
+  Serial.print(F(","));
+  // Latitude: 
+  sPrintData(message, 3, 15, 5);
+  Serial.print(F(","));
+  // Longditude:
+  sPrintData(message, 3, 18, 5);
+  
+  if(len > 21){
+    // Repeater Latitude
+    Serial.print(F(","));
+    sPrintData(message, 3, 22, 5);
+    Serial.print(F(","));
+    // Repeater Longditude
+    sPrintData(message, 3, 25, 5);
+  }
+  
+  Serial.println();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ FinalSystem/ReliableReceiver/ReliableReceiver.ino
@@ -0,0 +1,82 @@
+#include <RHReliableDatagram.h>
+#include <RH_RF95.h>
+#include <SPI.h>
+#include <LoRa.h>
+#include "systemConstants.h"
+#include <memoryReceiver.h>
+#include <EEPROM.h>
+#include <Wire.h>
+
+
+RH_RF95 lora(LoRa_CS, LoRa_INT);
+RHReliableDatagram manager(lora, RECEIVER_ADDRESS); // Instanciate this object with address.
+
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+  
+  while (!Serial) ; // Wait for serial port to be available
+  initializeLoRa();
+  if (initializeMemory()){
+    Serial.println(F("First time, initializing values to zero"));
+    delay(50);
+  }
+  /*
+  for (int i = 0; i < 4; i++){
+    packageMemory[i] = -1;
+  }
+  packageNum = 0;
+  packageMemoryPointer = 0;
+  */
+  //Serial.println(F("Signal Strength, Packet number, Temp, Humidity, Pressure, Debth, Time"));
+  Serial.println(F("Receiver on"));
+}
+
+
+
+void loop() {
+  if (manager.available()){
+    // Serial.println();
+    uint8_t bufLen = sizeof(buf); // Needs to be here to convert to uint8_t
+    uint8_t from;
+    bool duplicate;
+    
+    // We receive, and wait forever to receive it
+    bool receiveSuccess = receive(&duplicate, buf, &bufLen, &from, -1);
+    if (!receiveSuccess){
+      Serial.println("Receive not a success");
+    }
+    else if (duplicate && from != REPEATER_ADDRESS){
+      Serial.println(F("We got a duplicate: "));
+      writeDataToSerial(buf, (int)bufLen, from);
+    }
+    else if (receiveSuccess && !duplicate && from != REPEATER_ADDRESS){
+      writeMessageToMemory(buf, (int)bufLen, from);
+      writeDataToSerial(buf, (int)bufLen, from);
+    }
+    // The variable 'duplicate' is not in use yet.
+    
+    
+    /*
+    // After successful receive(s), send a message: 
+    if (receiveSuccess){
+      uint8_t data[] = "And hello back to you";
+      uint8_t numberedResponse[sizeof(packageNum) + sizeof(data)];
+      addPackageNum(&numberedResponse[0], &data[0], sizeof(data));
+      
+      if (!manager.sendtoWait(numberedResponse, sizeof(numberedResponse), from)){
+        // Serial.println("Sending failed");
+      }
+      else {
+        // Serial.print(F("Sending successful: "));
+        // Serial.print((int)numberedResponse[0]);
+        // Serial.println((char*)&numberedResponse[1]);
+        updatePackageNum();
+      }
+    }
+    */
+  }
+}
